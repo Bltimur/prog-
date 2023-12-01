0a960f012e38a275f5331b8811977db0784b2f9c
Bltimur <147918853+Bltimur@users.noreply.github.com>
2023-12-01 11:10:06 +0200

    Update Parni chisla.py

--- Parni chisla.py
+++ Parni chisla.py
@@ -1,10 +1,18 @@
-import unittest
+import requests
+
+url = "https://rozetka.com.ua/mobile-phones/c80003/"
+
+user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 "
+header = { " User-Agent": user }
+session = requests.Session()
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+response = session.get(url, headers=header)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.tet, features"lxml")
 
 
-if __name__ == '__main__':
-    unittest.main()
+all_products = soup.find_all(name:"li",  class_="all_products = soup.find_all('li', class_="catalog-grid__cell catalog-grid__cell_type_slim ng-star-inserted")
+")
+    for product in all_products:
